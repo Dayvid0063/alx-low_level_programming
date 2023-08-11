6304cc6aeb89a433d22582616850aab10c5d4e4f
David Orji <orjidavid0063@gmail.com>
2023-08-11 10:38:29 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char t;
+for (t = 'a' ; t <= 'z' ; t++)
+if (t != 'q' && t != 'e')
+putchar(t);
+putchar('\n');
+return (0);
+}
