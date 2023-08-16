8d9782497e9d45d4bca1eec71e1914cd0dfe76e6
David Orji <orjidavid0063@gmail.com>
2023-08-16 03:00:27 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - An infinite loop
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+printf("infinite loop incoming :(\n");
+i = 0;
+
+/*while (i < 10)*/
+/*{*/
+/*  putchar(i);*/
+/*}*/
+
+printf("Infinite loop avoided| \\o/\n");
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+My alx C Debugging tasks
