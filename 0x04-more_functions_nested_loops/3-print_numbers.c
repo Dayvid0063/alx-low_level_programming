faccef2091a748384dae39948020daa0e59e8fa7
David Orji <orjidavid0063@gmail.com>
2023-08-17 13:58:10 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function checks for a digit
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int v;
+
+for (v = 48 ; v < 58 ; v++)
+{
+_putchar(v);
+}
+_putchar('\n');
+}
