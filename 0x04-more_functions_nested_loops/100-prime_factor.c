9fb6ba4974767c6313dd38398db19e2121e49e3d
David Orji <orjidavid0063@gmail.com>
2023-08-17 18:06:50 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function find and print largest prime factor of num 612852475143
+ *
+ * Return: 0
+ */
+int main(void)
+{
+long prime = 612852475143, d;
+
+while (d < (prime / 2))
+{
+if ((prime % 2) == 0)
+{
+prime /= 2;
+continue;
+}
+for (d = 3 ; d < (prime / 2) ; d += 2)
+{
+if ((prime % d) == 0)
+prime /= d;
+}
+}
+printf("%ld\n", prime);
+return (0);
+}
