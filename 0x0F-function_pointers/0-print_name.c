c992b01d7e699febfc4daa79f5ea9b9a99a3d933
David Orji <orjidavid0063@gmail.com>
2023-09-13 05:31:10 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - func prints name
+ * @name: str being added
+ * @f: ptr to func
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
