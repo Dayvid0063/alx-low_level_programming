88423f69d87d1d05627b4a59d22ff6bd097168ca
Dayvid0063 <orjidavid0063@gmail.com>
2023-10-02 06:48:03 -0700

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file - func append text at the end of file
+ * @filename: Name of file
+ * @text_content: Contents of text
+ *
+ * Return: 1 if success,-1 if fail/NULL
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int u;
+int v;
+int w;
+if (!filename)
+return (-1);
+if (text_content != NULL)
+{
+for (v = 0; text_content[v];)
+v++;
+}
+u = open(filename, O_WRONLY | O_APPEND);
+w = write(u, text_content, v);
+if (u == -1 || w == -1)
+return (-1);
+close(u);
+return (1);
+}
