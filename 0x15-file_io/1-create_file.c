8b203d8e75e472757780f37b71551171bf5f0f25
Dayvid0063 <orjidavid0063@gmail.com>
2023-10-02 05:44:08 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - func creates a file
+ * @filename: Name of file
+ * @text_content: Content of texts
+ *
+ * Return: 1 if success, -1 on fail/NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+int u;
+int v;
+int w;
+if (!filename)
+return (-1);
+u = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (text_content != NULL)
+text_content = "";
+if (u == -1)
+return (-1);
+for (v = 0; text_content[v]; v++)
+;
+w = write(u, text_content, v);
+if (w == -1)
+return (-1);
+close(u);
+return (1);
+}
