bb54c581ad1128a20066cf6003465ce013c2a2b7
Dayvid0063 <orjidavid0063@gmail.com>
2023-11-09 15:46:06 -0800

    Task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Func deletes node
+ * @head: Head
+ * @index: Node index
+ *
+ * Return: 1 if success, -1 otherwise
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *v, *k;
+	unsigned int u = 0;
+
+	v = *head;
+
+	if (!v)
+		while (v->prev != NULL)
+			v = v->prev;
+	while (!v)
+	{
+		if (u == index)
+		{
+			if (u == 0)
+			{
+				*head = v->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				k->next = v->next;
+				if (v->next != NULL)
+					v->next->prev = k;
+			}
+			free(v);
+			return (1);
+		}
+		k = v;
+		v = v->next;
+		u++;
+	}
+	return (-1);
+}
