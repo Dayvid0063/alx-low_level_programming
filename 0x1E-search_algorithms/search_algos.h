dea0240b00a14171aaf0639c78c3c708b045fc91
Dayvid0063 <orjidavid63@gmail.com>
2024-03-12 12:35:19 +0100

    exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,8 +44,10 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 /* Helper Prototypes */
 int r_search(int *array, size_t begin, size_t end, int value);
+int b_search(int *array, size_t begin, size_t end, int value);
 
 #endif
